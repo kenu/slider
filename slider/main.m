972f47ad15c20623a7c1667403c703d8d4cfef7c
kenu <kenu@okjsp.pe.kr>
2011-08-17 03:53:39 +0900

    Initial Commit

---
+++ slider/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  slider
+//
+//  Created by kenu heo on 11. 8. 17..
+//  Copyright 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
---
+++ slider/sliderAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  sliderAppDelegate.h
+//  slider
+//
+//  Created by kenu heo on 11. 8. 17..
+//  Copyright 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sliderAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
